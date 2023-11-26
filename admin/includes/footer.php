3974ec942bc4c3fe74cffcf58d78f467ba1f5a75
mutooro <martin.mutooro@gmail.com>
2023-11-26 16:17:12 +0300

    bb

--- admin/includes/footer.php
+++ admin/includes/footer.php
@@ -2,5 +2,5 @@
     <div class="pull-right hidden-xs">
       <b>All rights reserved</b>
     </div>
-    <strong>Copyright &copy; 2020</a></strong>
+    <strong>Copyright &copy; <?php echo date('Y'); ?> Muto oro</strong>
 </footer>
